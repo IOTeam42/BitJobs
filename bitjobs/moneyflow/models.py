b3406a5a9d18759f2cbfaa4210c70473ab7a765b
Jarosław Ławnicki <jaroslawlawnicki@gmail.com>
2017-03-31 19:11:06 +0200

    Added a few methods for Wallet model

--- bitjobs/moneyflow/models.py
+++ bitjobs/moneyflow/models.py
@@ -3,9 +3,17 @@ Module for storing data about user's money, payments and user's transaction hist
 
 from __future__ import unicode_literals
 
-from django.db import models
+from django.db import models, transaction
 
 from bitjobs.settings import CURRENCIES
+
+class CannotTransfer(Exception):
+    def __init__(self, message):
+        super(CannotTransfer, self).__init__(message)
+
+
+def valid_currency(name):
+    return name in [currency for (currency, _) in CURRENCIES]
 
 
 class Wallet(models.Model):
@@ -13,6 +21,32 @@ class Wallet(models.Model):
         super(Wallet, self ).save(*args, **kwargs)
         for (c, c2) in CURRENCIES:
             CurrencyAccount.objects.create(currency=c, amount=0, wallet=self)
+
+    def _currency_account_by_currency(self, currency):
+        return self.currencyaccount_set.filter(currency__exact=name).first()
+
+    def money_by_currency(self, currency:str):
+        if not valid_currency(name)
+            raise ValueError("Invalid currency name: '%s'" % currency)
+        return self._currency_account_by_currency(currency).amount
+
+    #TODO: Shouldn't that go somewhere else?
+    def transfer(self, target, currency, quantity):
+        if not valid_currency(currency):
+            raise ValueError("Invalid currency name '%s'" % currency)
+
+        source_currency_account = self._currency_account_by_currency(currency)
+        money = source_currency_account.amount
+        if money < quantity:
+            raise CannotTransfer("Insufficient money, needs %d %s, have only %d %s" % (quantity, currency,money, currency ))
+
+        target_currency_account = target._currency_account_by_currency(currency)
+
+        with transaction.atomic():
+            target_currency_account.amount += quantity
+            target_currency_account.save()
+            source_currency_account.amount -= quantity
+            source_currency_account.save()
 
 
 # Unfortunately django-money does not allow selecting specific currency, so I'm rolling my own solution
