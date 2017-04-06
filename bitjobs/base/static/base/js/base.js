e2b8a0cfd43e46ba2dee32db03fc5be1f4d1c313
filip <plata12@wp.pl>
2017-04-06 23:37:40 +0200

    Stub searchbox, navbar more or less completed

--- bitjobs/base/static/base/js/base.js
+++ bitjobs/base/static/base/js/base.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+
+    function search(commission_id) {
+      // here we have id of model and we need an url
+      window.location.href = 'TODO url';
+    }
+
+    $(function() {
+      $("#bitjobs-search-input").autocomplete({
+          source: "{% url 'TODO' %}",
+          minLength: 2,
+          select: function(event, ui) {
+              search(ui.item.id);
+          }
+      });
+    });
+
+
+    // stuff below is not working, its late, will fix tomorrow
     var navLinks = document.querySelector('.navNarrow');
     var narrowLinks = document.querySelector('.narrowLinks');
 
