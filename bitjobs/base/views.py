dbb1f1312617814f1859d015fbf726e2e4b9d4e7
filip <plata12@wp.pl>
2017-04-07 00:03:01 +0200

    Basic register, login not working, will do it tomorrow

--- bitjobs/base/views.py
+++ bitjobs/base/views.py
@@ -1,12 +1,26 @@
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.contrib.auth.models import User
+
+from registration.views import RegistrationView
+from registration.signals import user_registered
+
 from bargainflow.models import Commission
 from bargainflow.forms import CommissionForm
 
 
-class RegisterView(TemplateView):
-    template_name = "base/templates/registration/registration.html"
+class RegisterView(RegistrationView):
+
+    success_url = "/"
+
+    def register(self, form):
+        data = form.cleaned_data
+        username = data['username']
+        password = data['password1']
+        email = data['email']
+        user = User.objects.create_user(username, email, password)
+        return user
 
 
 class LoginView(TemplateView):
