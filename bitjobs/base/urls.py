d57f66d96102ed11841e0f5e01f38c46c835d92f
filip <plata12@wp.pl>
2017-04-07 19:19:01 +0200

    Registration-login workflow is completed (checked - email is sent on debug server), for heroku we will need a provider

--- bitjobs/base/urls.py
+++ bitjobs/base/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import url, include
 from registration.backends.default.views import RegistrationView
 from base import views
 
+
 urlpatterns = [
     url(r'^rejestracja/', views.RegisterView.as_view(),
         name="registration"),
-    #url(r'^logowanie/', views.LoginView.as_view(),
-    #    name="login"),
     url(r'^oferta/(?P<id>[0-9]+)$', views.CommissionView.as_view(),
         name="commission-detail"),
     url(r'^oferty/$', views.CommissionDashboardView.as_view(),
@@ -15,9 +14,6 @@ urlpatterns = [
         name="home"),
     url(r'^oferta/', views.CommissionAddView.as_view(),
         name="add-commission"),
-    # Fallback switching registration form
-    #url(r'^accounts/register/$', RegistrationView.as_view(form_class=)
-    #        , name='registration_register'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^accounts_auth/', include('django.contrib.auth.urls')),
 ]
