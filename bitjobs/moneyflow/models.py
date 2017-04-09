10ea0b68fd0c0f074f0fff61da58ce421b35a400
Jarosław Ławnicki <jaroslawlawnicki@gmail.com>
2017-04-09 14:45:47 +0200

    Fixed duplicate creation of CurrencyAccount for single Wallet

--- bitjobs/moneyflow/models.py
+++ bitjobs/moneyflow/models.py
@@ -8,6 +8,7 @@ from django.core import validators
 
 from bitjobs.settings import CURRENCIES
 
+
 class CannotTransfer(Exception):
     def __init__(self, message):
         super(CannotTransfer, self).__init__(message)
@@ -19,19 +20,20 @@ def valid_currency(name):
 
 class Wallet(models.Model):
     def save(self, *args, **kwargs):
-        super(Wallet, self ).save(*args, **kwargs)
+        super(Wallet, self).save(*args, **kwargs)
         for (c, c2) in CURRENCIES:
-            CurrencyAccount.objects.create(currency=c, amount=0, wallet=self)
+            if not self.currencyaccount_set.filter(currency=c).exists():
+                CurrencyAccount.objects.create(currency=c, amount=0, wallet=self)
 
     def _currency_account_by_currency(self, currency):
         return self.currencyaccount_set.filter(currency__exact=currency).first()
 
-    def money_by_currency(self, currency:str):
+    def money_by_currency(self, currency: str):
         if not valid_currency(currency):
             raise ValueError("Invalid currency name: '%s'" % currency)
         return self._currency_account_by_currency(currency).amount
 
-    #TODO: Shouldn't that go somewhere else?
+    # TODO: Shouldn't that go somewhere else?
     def transfer(self, target, currency, quantity):
         if not valid_currency(currency):
             raise ValueError("Invalid currency name '%s'" % currency)
@@ -39,7 +41,8 @@ class Wallet(models.Model):
         source_currency_account = self._currency_account_by_currency(currency)
         money = source_currency_account.amount
         if money < quantity:
-            raise CannotTransfer("Insufficient money, needs %d %s, have only %d %s" % (quantity, currency,money, currency ))
+            raise CannotTransfer(
+                "Insufficient money, needs %d %s, have only %d %s" % (quantity, currency, money, currency))
 
         target_currency_account = target._currency_account_by_currency(currency)
 
@@ -53,5 +56,9 @@ class Wallet(models.Model):
 # Unfortunately django-money does not allow selecting specific currency, so I'm rolling my own solution
 class CurrencyAccount(models.Model):
     currency = models.CharField(choices=CURRENCIES, max_length=3)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[validators.MinValueValidator(0)], default=0)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[validators.MinValueValidator(0)],
+                                 default=0)
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("currency", "wallet"),)
