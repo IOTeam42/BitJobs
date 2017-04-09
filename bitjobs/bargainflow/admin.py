5fe337a4e561665463a414af7554107344f83a4c
Kisieland <ddanielloo1996@gmail.com>
2017-04-09 10:25:03 +0200

    Corrected linking to tag search, problem is it doesn't find right commissions.

--- bitjobs/bargainflow/admin.py
+++ bitjobs/bargainflow/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Commission
+from .models import Commission, CommissionBid
 
 admin.site.register(Commission)
+admin.site.register(CommissionBid)
